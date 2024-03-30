9e4e570ee10d5432aa2cf4f3ee8126dc0f6f820d
Vladislav <neo_120@mail.ru>
2024-03-30 19:56:21 +0300

    Deploy scripts fixed

--- Deploy/scripts/update.sh
+++ Deploy/scripts/update.sh
@@ -6,8 +6,8 @@
 cd "$(dirname "$0")" || exit 1
 
 # prepare temp directories
-test ./passmeta-tmp && rm -r ./passmeta-tmp
-test ./passmeta-new && rm -r ./passmeta-new
+test -e ./passmeta-tmp && rm -r ./passmeta-tmp
+test -e ./passmeta-new && rm -r ./passmeta-new
 
 # get and unzip
 wget -P ./passmeta-tmp https://github.com/vlad120/PassMeta-Server/archive/refs/heads/master.zip || exit 1
@@ -19,7 +19,9 @@ cp -a ./passmeta-tmp/PassMeta-Server-master/App ./passmeta-new/App || exit 1
 cp -a ./passmeta-tmp/PassMeta-Server-master/Deploy ./passmeta-new/Deploy || exit 1
 cp ./passmeta-tmp/PassMeta-Server-master/main.py ./passmeta-new/main.py || exit 1
 cp ./passmeta-tmp/PassMeta-Server-master/requirements.txt ./passmeta-new/requirements.txt || exit 1
+cp ./passmeta-tmp/PassMeta-Server-master/Dockerfile ./passmeta-new/Dockerfile || exit 1
 cp ./passmeta/Deploy/.env.local ./passmeta-new/Deploy/.env.local
+rm -r ./passmeta-tmp
 
 # stop services
 bash ./passmeta/Deploy/scripts/stop.sh || exit 1
@@ -29,10 +31,13 @@ rm -f -r ./passmeta-old
 mv ./passmeta ./passmeta-old || exit 1
 mv ./passmeta-new ./passmeta || exit 1
 
-# clear temp directories
-rm -r ./passmeta-tmp
-rm -r ./passmeta-new
-
-# rebuild & run container
-bash ./passmeta/Deploy/scripts/rebuild.sh
+# rebuild & start container
+bash ./passmeta/Deploy/scripts/rebuild.sh || exit 1
+echo "----"
+echo "Successfully updated!"
+read -p "Start services now? (Y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 bash ./passmeta/Deploy/scripts/start.sh
+fi
